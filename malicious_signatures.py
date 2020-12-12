90933a2cbad95f8d13e192964615dd0de6801c47
jaalma <james@jmaclachlan.me>
2020-12-12 09:39:38 +0000

    add malicious file signatures

---
+++ malicious_signatures.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+malicious_indicators = {
+	'pe_base64': 'TVqQA',
+	'pe_base64_2': 'TVpQ',
+	'pe': 'MZ',
+	'base64_gz': 'H4sI'
+	'elf64': 'f0VMR',
+	'sh_script': '#!/bin/bash',
+	'powershell': 'powershell',
+	'powershell_webclient': 'Net.WebClient',	
+	'powershell_script': 'Invoke-',
+	'vba': 'Auto_Open()',
+	'vbs': 'wcript.shell''
+}
